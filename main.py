e6813a026406e5d6be46ff0fcb45378cced15a24
AdeJurca <adelina.jurca@gmail.com>
2022-08-11 11:12:30 +0300

    interpreter added

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ from models.Tool import Tool
 
 
 def get_by_name(value: str) -> Tool:
+    """
+    display a tool from data.json local file by giving the name of the tool as paramter
+    """
     with open("./data/data.json", 'r') as file:
         data = json.load(file)
-        tools: List[Tool] = [Tool(**item) for item in data]
+        tools: List[Tool] = []
+        for item in data:
+            tools.append(Tool(**item))
         for tool in tools:
             if tool.name == value:
                 print(tool.json())
@@ -18,26 +23,33 @@ def get_by_name(value: str) -> Tool:
 
 
 def get_all_tools() -> List[Tool]:
+    """
+    display the list of tools from data.json
+    """
     with open("./data/data.json") as file:
         data = json.load(file)
-        tools: List[Tool] = [Tool(**item) for item in data]
+        tools: List[Tool] = []
+        for item in data:
+            tools.append(Tool(**item))
         print("---")
         for tool in tools:
             print(tool.json())
         return tools
 
 def get_tool_by_url(url: str) -> Tool:
+    """display the in json format from the bio.tools url of the tool"""
     response = requests.get(url)
-    tool: Tool = Tool(**json.loads(response.text))
+    dict_obj = json.loads(response.text)
+    tool: Tool = Tool(**dict_obj)
     print(tool.json())
     print(tool)
     return tool
 
 
 if __name__ == "__main__":
-    # tool = get_by_name("AEGeAn")
-    # print(tool)
-    t = get_all_tools()
-    print(t)
+    tool = get_by_name("AEGeAn")
+    print(tool)
+    # t = get_all_tools()
+    # print(t)
     # tool = get_tool_by_url("https://bio.tools/api/jaspar?format=json")
     # print("Printing tool from main: ", tool)
--- models/Tool.py
+++ models/Tool.py
@@ -1,6 +1,5 @@
-import json
 import string
-from typing import List, Optional, Any
+from typing import List, Optional
 
 import pydantic
 from pydantic import BaseModel
@@ -25,20 +24,20 @@ class Tool(BaseModel):
     version: List[str]
     otherID: Optional[List[OtherID.OtherID]] = []
     relation: List[str]
-    function: List[Function.Function]
+    function: List[Function.Function] = []
     toolType: Optional[List[str]] = [] #ar trebuie default lista goala peste tot
     topic: List[Topic.Topic]
-    operatingSystem: List[str]
-    language: List[str]
+    operatingSystem: List[str] = []
+    language: List[str] = []
     license: str
-    collectionID: List[str]
-    elixirPlatform: List[str]
-    elixirNode: List[str]
-    link: List[Any]
-    download: List[Any]
-    publication: Optional[List[Publication.Publication]]
-    credit: List[Any]
-    confidence_flag: str=None
+    collectionID: List[str] = []
+    elixirPlatform: List[str] = []
+    elixirNode: List[str] = []
+    link: List[str] = []
+    download: List[str] = []
+    publication: Optional[List[Publication.Publication]] = []
+    credit: List[str] = []
+    confidence_flag: str = None
 
     #def toJSON(self):
         #return json.dumps(self, default=lambda o: o.__dict__,
@@ -47,6 +46,8 @@ class Tool(BaseModel):
     @pydantic.validator('name')
     @classmethod
     def name_validator(cls, value):
+        """CustomError raises 2 exceptions regarding the length of the name of the tool(the name needs to have at least
+        one character and cannot be longer than 20 characters)"""
         if len(value) == 0:
             raise CustomError(value=value, message='Name cannot be empty')
         if len(value) > 20:
@@ -57,6 +58,8 @@ class Tool(BaseModel):
     @pydantic.validator('description')
     @classmethod
     def description_validator(cls, value):
+        """CustomError raises 2 exceptions regarding the length of the description of the tool(the description needs to
+        have at least five characters and cannot be longer than 500 characters)"""
         if len(value) > 500:
             raise CustomError(value=value, message='Description is to long')
         if len(value) < 5:
@@ -67,6 +70,8 @@ class Tool(BaseModel):
     @pydantic.validator('version')
     @classmethod
     def version_validator(cls, value):
+        """CustomError raises 2 exceptions regarding the message of the version of the tool(the version cannot contain
+        whitespaces or special characters"""
         for v in value:
             if any(p in v for p in string.whitespace):
                 raise CustomError(value=value, message='Version number must not contain whitespaces')
@@ -78,6 +83,9 @@ class Tool(BaseModel):
     @pydantic.validator('license')
     @classmethod
     def license_validator(cls, value):
+        """CustomError raises an exception regarding the fact that this license needs to be part of the list of
+        commonly found licenses and exceptions -spdx- used in free and open source and other collaborative software or
+        documentation"""
         if value not in spdx_license_list.LICENSES:
             raise CustomError(value=value, message=f'{value} is not a valid license')
 
